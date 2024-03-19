3faeb0dba764382c719365eb1aecc17c90208ca8
Lucas Miguel <lucasrsj.miguel@gmail.com>
2024-03-19 17:53:31 -0300

    Loop funcionando pela primeira vez

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func checkStatus(url string) string {
+	response, err := http.Get(url)
+	if err != nil {
+		return err.Error()
+	}
+	defer response.Body.Close()
+	return response.Status
+}
 
 func main() {
-	fmt.Println("\tHYDRA\nVer. 0.0.1")
+	var domain string
+	fmt.Println("                           _____     ____      ___")
+	fmt.Println("        |\\  |\\   |\\   /|  |  _  \\   |  _ \\    / _ \\")
+	fmt.Println("        | | | |  | | | |  | | \\  |  | | \\ \\  | | | |")
+	fmt.Println("        | |_| |  \\ \\_/ /  | |  | |  | |_/ /  | |_| |")
+	fmt.Println("        |  _  |   \\   /   | |  | |  |  _ /   |  _  |")
+	fmt.Println("        | | | |    | |    | |  | |  | |\\ \\   | | | |")
+	fmt.Println("        | | | |    | |    | |_/  |  | | \\ \\  | | | |")
+	fmt.Println("         \\|  \\|    \\ /    |_____/   |/   \\/  |/   \\|")
+	fmt.Println("\nVer. 0.0.1")
+	fmt.Print("\n\nURL: ")
+	fmt.Scan(&domain)
+
+	file, errF := os.Open("namelist.txt")
+	if errF != nil {
+		log.Fatal(errF)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	response := checkStatus("https://www." + domain)
+	fmt.Println(domain + "/" + scanner.Text() + "\t:" + response)
+	for scanner.Scan() {
+		response := checkStatus("https://www." + domain + "/" + scanner.Text())
+		fmt.Println(domain + "/" + scanner.Text() + "\t\t:" + response)
+	}
+
 }
