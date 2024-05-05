5a8b2629667b446997787e6bfc369a4361571de4
Lucas Miguel <lucasrsj.miguel@gmail.com>
2024-05-05 15:15:40 -0300

    Adicionado os arquivos de texto e o diretório pkg com os packages do programa. Main foi modificada para comportar o Menu inicial e agora é possivel indicar qual wl deseja usar e onde fazer o fuzz na URL

--- main.go
+++ main.go
@@ -4,44 +4,89 @@ import (
 	"bufio"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
+	"strings"
+	"sync"
+
+	uiterminal "./pkg/UIterminal"
+	mod "./pkg/modulos"
 )
 
-func checkStatus(url string) string {
-	response, err := http.Get(url)
-	if err != nil {
-		return err.Error()
+func ReadCommand(command string) (url string, wl string) {
+
+	cmd := command
+
+	fmt.Println("\n" + command)
+
+	if idx := strings.Index(cmd, " -URL "); idx != -1 || strings.Index(cmd, "-URL ") == 0 {
+		idx++
+
+		idx2 := idx + 5 //idx2 define o index onde a url começa
+		for cmd[idx2] == 32 {
+			idx2++
+		} //busca início da url
+		idx3 := idx2 + 1
+		for cmd[idx3] != 32 {
+			if idx3 == len(cmd)-1 {
+				idx3++
+				break
+			}
+			idx3++
+		} //busca fim da url
+		url = cmd[idx2:idx3]
+		cmd = cmd[:idx] + cmd[idx3:]
 	}
-	defer response.Body.Close()
-	return response.Status
+	fmt.Println("command: " + command)
+	fmt.Println("cmd: " + cmd)
+	fmt.Println("url: " + url)
+
+	if idx := strings.Index(cmd, " -wl "); idx != -1 || strings.Index(cmd, "-wl ") == 0 {
+		idx++
+
+		idx2 := idx + 4 //idx2 define o index onde a wl começa
+		for cmd[idx2] == 32 {
+			idx2++
+		} //busca início da wl
+		idx3 := idx2 + 1
+		for cmd[idx3] != 32 {
+			if idx3 == len(cmd)-1 {
+				idx3++
+				break
+			}
+			idx3++
+		} //busca fim da wl
+		wl = cmd[idx2:idx3]
+		cmd = cmd[:idx] + cmd[idx3:]
+	}
+
+	fmt.Println("command: " + command)
+	fmt.Println("cmd: " + cmd)
+	fmt.Println("wl: " + wl)
+
+	return
 }
 
 func main() {
 	var domain string
-	fmt.Println("                           _____     ____      ___")
-	fmt.Println("        |\\  |\\   |\\   /|  |  _  \\   |  _ \\    / _ \\")
-	fmt.Println("        | | | |  | | | |  | | \\  |  | | \\ \\  | | | |")
-	fmt.Println("        | |_| |  \\ \\_/ /  | |  | |  | |_/ /  | |_| |")
-	fmt.Println("        |  _  |   \\   /   | |  | |  |  _ /   |  _  |")
-	fmt.Println("        | | | |    | |    | |  | |  | |\\ \\   | | | |")
-	fmt.Println("        | | | |    | |    | |_/  |  | | \\ \\  | | | |")
-	fmt.Println("         \\|  \\|    \\ /    |_____/   |/   \\/  |/   \\|")
-	fmt.Println("\nVer. 0.0.1")
-	fmt.Print("\n\nURL: ")
-	fmt.Scan(&domain)
+	var wg sync.WaitGroup
+	var wl string
+	var threads = 4
 
-	file, errF := os.Open("namelist.txt")
+	command := uiterminal.InitMenu()
+
+	domain, wl = ReadCommand(command)
+
+	file, errF := os.Open(wl)
 	if errF != nil {
 		log.Fatal(errF)
 	}
 	defer file.Close()
+
 	scanner := bufio.NewScanner(file)
-	response := checkStatus("https://www." + domain)
-	fmt.Println(domain + "/" + scanner.Text() + "\t:" + response)
-	for scanner.Scan() {
-		response := checkStatus("https://www." + domain + "/" + scanner.Text())
-		fmt.Println(domain + "/" + scanner.Text() + "\t\t:" + response)
-	}
+	fmt.Println("-------------------------------------------------------------")
+	wg.Add(threads)
+	mod.RunFUZZ(threads, domain, scanner, &wg)
+	fmt.Println("\n-------------------------------------------------------------")
+	fmt.Println("\n\nFim do Processo")
 
 }
