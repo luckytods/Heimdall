2dad14d8f6b476d3a2bf9ef277681b53f11cbf47
luckytods <lucasrsj.miguel@gmail.com>
2024-09-04 15:37:27 -0300

    Pasta para separação dos modulos

---
+++ agent/config.py
@@ -0,0 +1,12 @@
+DB_CONFIG = {
+    'user': 'admin',
+    'password': '1Ringt0rul3th3mall',
+    'host': 'heimdallmonitoring.chkyymc4suru.us-east-2.rds.amazonaws.com',
+    'database': 'Heimdall_monitoring'
+}
+
+community = "Heimdall"  # Comunidade SNMP configurada 
+
+ip_list = [
+        "192.168.0.17",  # IP do Windows desktop
+    ]
